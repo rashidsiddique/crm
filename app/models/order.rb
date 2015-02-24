1e16a1a39bc83e1f0698506b9e78967d97bc02e9
rashid <rashid.siddique@pikessoft.com>
2015-02-24 19:28:34 +0500

    set order product associations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,3 @@
 class Order < ActiveRecord::Base
-  has_and_belongs_to_many :products
+  has_and_belongs_to_many :products, join_table: :order_products
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories, join_table: :product_categories
+  has_and_belongs_to_many :orders, join_table: :order_products
 end
