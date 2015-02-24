f875e1684a814f7914b6783549946e4bfcb09263
rashid <rashid.siddique@pikessoft.com>
2015-02-24 19:21:54 +0500

    set associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,3 @@
 class Product < ActiveRecord::Base
-  has_and_belongs_to_many :categories
-  has_and_belongs_to_many :orders
+  has_and_belongs_to_many :categories, join_table: :product_categories
 end
